cf02f08798770bf573fb52c9970d78d1b9a253ad
ZSedillo <sedillozandro720@gmail.com>
2025-04-04 22:13:29 +0800

    test

--- server/util/putObject.js
+++ server/util/putObject.js
@@ -1,36 +1,32 @@
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-const s3Client = new S3Client({
-    region: process.env.AWS_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_KEY,
-    }
-});
+const mime = require("mime-types"); // Import mime-types package
 
 exports.putObject = async (file, fileName) => {
     try {
+        console.log("MIME Type:", file.mimetype); // Debugging: Check the MIME type
+
+        const contentType = file.mimetype || mime.lookup(fileName) || "application/octet-stream"; // Ensure a valid MIME type
+
         const params = {
             Bucket: process.env.AWS_S3_BUCKET,
             Key: fileName,
-            Body: file,
-            ContentType: "image/jpg,jpeg,png",
+            Body: file.buffer || file, 
+            ContentType: contentType, 
         };
+
+        console.log("Uploading with ContentType:", contentType); // Debugging: See what content type is being sent to S3
 
         const command = new PutObjectCommand(params);
         const data = await s3Client.send(command);
 
         if (data.$metadata.httpStatusCode !== 200) {
+            console.error("Upload failed:", data);
             return;
         }
 
-        let url = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${params.Key}`;
-        console.log(url);
+        const url = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${params.Key}`;
+        console.log("Uploaded URL:", url);
         return { url, key: params.Key };
     } catch (err) {
-        console.log(err);
+        console.error("Error uploading to S3:", err);
     }
 };
