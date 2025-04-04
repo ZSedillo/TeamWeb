8133f79315c68c1f58c9c04e9a6540f60161bde7
ZSedillo <sedillozandro720@gmail.com>
2025-04-04 23:14:19 +0800

    removing old comments

--- server/controllers/homepageController.js
+++ server/controllers/homepageController.js
@@ -7,23 +7,6 @@ const { putObject } = require('../util/putObject');
 const { deleteObject } = require('../util/deleteObject');
 const url = require('url'); // For URL parsing
 
-// Upload Image
-// const uploadImage = async (req, res) => {
-//     if (!req.file) return res.status(400).json({ message: "No file uploaded" });
-
-//     try {
-//         const newImage = new Homepage({
-//             image_url: req.file.filename, // Save only filename
-//             created_at: new Date(),
-//         });
-
-//         await newImage.save();
-//         res.status(201).json({ message: "Image uploaded successfully", image: newImage });
-//     } catch (error) {
-//         console.error("Error saving image:", error);
-//         res.status(500).json({ message: "Error saving image to database" });
-//     }
-// };
 const uploadImage = async (req, res) => {
     if (!req.files || !req.files.image) {
         return res.status(400).json({ message: "No file uploaded" });
@@ -52,35 +35,6 @@ const uploadImage = async (req, res) => {
         res.status(500).json({ message: "Error saving image to database" });
     }
 };
-
-
-
-// Delete Image
-// const deleteImage = async (req, res) => {
-//     try {
-//         const { filename } = req.params;
-
-//         // Find and delete from MongoDB
-//         const deletedImage = await Homepage.findOneAndDelete({ image_url: filename });
-
-//         if (!deletedImage) {
-//             return res.status(404).json({ message: "Image not found in database" });
-//         }
-
-//         // Delete from server storage
-//         const filePath = path.join(__dirname, "../homepage", filename);
-//         fs.unlink(filePath, (err) => {
-//             if (err) {
-//                 console.error("Error deleting file:", err);
-//                 return res.status(500).json({ message: "Failed to delete file from server" });
-//             }
-//             res.json({ message: "Image deleted successfully" });
-//         });
-//     } catch (error) {
-//         console.error("Error deleting image:", error);
-//         res.status(500).json({ message: "Error deleting image" });
-//     }
-// };
 
 const deleteImage = async (req, res) => {
     try {
