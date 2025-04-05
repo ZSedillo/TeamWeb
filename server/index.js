62db44e064a7c1700f13437247f91f86cfb5906a
ZSedillo <sedillozandro720@gmail.com>
2025-04-05 22:58:51 +0800

    updating cors

--- server/index.js
+++ server/index.js
@@ -19,7 +19,14 @@ const app = express()
 const router = express.Router();
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:5173", "https://your-frontend-domain.com"], // adjust this
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true
+  }));
+  
 
 connectDB()
 
