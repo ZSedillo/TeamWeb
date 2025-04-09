dfdcf48d056c1481f4880226581665ea437b41b5
ZSedillo <sedillozandro720@gmail.com>
2025-04-10 00:21:56 +0800

    removing send reset code

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -14,6 +14,5 @@ router.post('/edit-password', authenticate, userController.editPassword);
 router.post('/update-user-info', authenticate, userController.updateUserInfo);
 router.delete('/delete-account', authenticate, userController.deleteAccount);
 router.get('/current-user', authenticate, userController.getCurrentUser);
-router.post('/send-reset-code', userController.sendResetCode);
 
 module.exports = router;
