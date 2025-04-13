c92b65a1e57c0e8c0e7a452df7dea3e6cc30356d
ZSedillo <sedillozandro720@gmail.com>
2025-04-13 16:54:59 +0800

    adding /search-reports

--- server/routes/reportRoutes.js
+++ server/routes/reportRoutes.js
@@ -1,11 +1,12 @@
 // routes/reportRoutes.js
 const express = require('express');
-const { viewReports, addReport, deleteReports } = require('../controllers/reportController');
+const { viewReports, searchReports, addReport, deleteReports } = require('../controllers/reportController');
 
 const router = express.Router();
 
 // Routes
 router.get('/view-report', viewReports);
+router.get('/search-reports', searchReports);
 router.post('/add-report', addReport);
 router.delete('/delete-reports', deleteReports);
 
