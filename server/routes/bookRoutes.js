17ec34122c93ff81fc4c7dcac91087cc41b93fe8
ZSedillo <sedillozandro720@gmail.com>
2025-05-28 20:27:17 +0800

    adding auth to bookRoutes

--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { getBookingAvailability, addBookingAvailability, editBookingAvailability, getAllBookings } = require("../controllers/bookController");
+const authenticate = require('../middleware/authMiddleware'); // Import the middleware
 
 const router = express.Router();
 
-router.get("/bookingAvailability", getBookingAvailability);
-router.post("/addBookingAvailability", addBookingAvailability);
-router.put("/editBookingAvailability/:id", editBookingAvailability);
 router.get("/getBookings", getAllBookings);
+
+// Protected Routes (require login)
+router.get("/bookingAvailability", authenticate, getBookingAvailability);
+router.post("/addBookingAvailability", authenticate, addBookingAvailability);
+router.put("/editBookingAvailability/:id", authenticate, editBookingAvailability);
 
 module.exports = router;
