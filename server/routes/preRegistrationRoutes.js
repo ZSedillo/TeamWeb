96e5f96045659cf48065c9f72e33d9470d9b42a1
ZSedillo <sedillozandro720@gmail.com>
2025-05-28 19:45:13 +0800

    removing /view-preregistration and changed it back to /

--- server/routes/preRegistrationRoutes.js
+++ server/routes/preRegistrationRoutes.js
@@ -19,7 +19,7 @@ router.post('/add', addPreRegistration);
 router.post('/addBooking', addBooking);
 
 // Protected routes
-router.get('/view-preregistration', authenticate, getPreRegistrations);
+router.get('/', authenticate, getPreRegistrations);
 router.put('/status/:id', authenticate, updatePreRegistrationStatus);
 router.put('/enrollment/:id', authenticate, updatePreregistrationEnrollmentStatus);
 router.get('/enrolled', authenticate, getEnrolledPreRegistrations);
