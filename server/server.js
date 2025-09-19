91e5af1376895cb4be24b69c08f58a5a790f6d66
ZSedillo <sedillozandro720@gmail.com>
2025-09-20 01:33:06 +0800

    removing limiter

--- server/server.js
+++ server/server.js
@@ -37,12 +37,12 @@ app.use((req, res, next) => {
 app.use(helmet());
 
 // Rate limiter (to prevent abuse)
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // max requests per IP
-  message: "Too many requests from this IP, please try again later.",
-});
-app.use(limiter);
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100, // max requests per IP
+//   message: "Too many requests from this IP, please try again later.",
+// });
+// app.use(limiter);
 
 // JSON body parser
 app.use(express.json());
