47a2eb3202b14af4b4cab11ac9bba0634f0462f2
ZSedillo <sedillozandro720@gmail.com>
2025-12-05 16:27:00 +0800

    env has contents but won't push the env to production

--- server/user/user.model.js
+++ server/user/user.model.js
@@ -36,11 +36,8 @@ const userSchema = new mongoose.Schema({
 // Ensure only one Head Admin exists
 userSchema.pre('save', async function (next) {
   if (this.role === 'head_admin') {
-    // We only check this if the role is actually being modified or it's a new user
-    // to prevent errors on simple updates
     if (this.isModified('role') || this.isNew) {
         const existingHeadAdmin = await mongoose.model('User').findOne({ role: 'head_admin' });
-        // If an existing head admin exists and it is NOT this current document
         if (existingHeadAdmin && existingHeadAdmin._id.toString() !== this._id.toString()) {
             throw new Error('There can only be one Head Admin.');
         }
